05a8b8c061e83057aa4f7c4d7c72508204d3bbcd
Simjvg <simjvg1987@gmail.com>
2023-11-18 13:17:07 -0500

    Iniciado

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,10 +1,14 @@
-const calculator = document.querySelector(`.calculator`);
-const keys = document.querySelector(`.calculator_keys`);
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.calculator_keys');
 
-keys.addEventListener(`click`, e => {
+keys.addEventListener('click', e => {
     if (e.target.matches(button)) {
+        const key = e.target;
+        const action = key.dataset.action;
         
+        if (!action){
+            console.log("number key!")
+        };
+        //if ( action === )  
     }
-})
-const key = e.target
-const action = key.dataset.action
+});
